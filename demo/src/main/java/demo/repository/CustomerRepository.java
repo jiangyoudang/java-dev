0efc618e4d382937c2ede2c885c6aa653770e317
cong <cong@cc>
2018-06-24 23:59:36 -0400

    restful

--- demo/src/main/java/demo/repository/CustomerRepository.java
+++ demo/src/main/java/demo/repository/CustomerRepository.java
@@ -3,8 +3,12 @@ package demo.repository;
 import demo.model.Customer;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+//public interface CustomerRepository extends CrudRepository<Customer, Long> {
+public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
 
 //  List<Customer> findAll();
+  List<Customer> findByLastname(@Param("name") String name);
 }
